62b91acfa16a9d772d69f87d2cb72dd43b2c7544
BFlorian59 <florianbroeks59@gmail.com>
2022-06-03 14:58:08 +0200

    étape 1

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,7 +1,7 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, id, city , country , tagline , price, portrait } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/photographers/Photographers ID Photos/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
@@ -9,9 +9,18 @@ function photographerFactory(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const ville =document.createElement('b');
+        ville.textContent = city;
+        const tag =document.createElement('b');
+        tag.textContent = tagline;
+        const prix =document.createElement('b');
+        prix.textContent = price+ "€/jour";
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(ville);
+        article.appendChild(tag);
+        article.appendChild(prix);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, id, city , country , tagline , price, portrait, getUserCardDOM }
 }
